045a9b43830e8d0c4e877fb5b1260af89cc1d21b
Yannjoel <yannj96@googlemail.com>
2016-10-13 10:52:00 +0200

    initialization file for the database

---
+++ PixYel_Backend/src/pixyel_backend/database/DatabaseInitializer.java
@@ -0,0 +1,20 @@
+package pixyel_backend.database;
+
+import java.sql.Connection;
+import java.sql.Statement;
+import pixyel_backend.database.MysqlConnector;
+
+public class DatabaseInitializer {
+	public static void init() throws Exception {
+		Connection conn = MysqlConnector.connectToDatabaseUsingPropertiesFile();
+		Statement statments = conn.createStatement();
+		statments.executeUpdate("DROP DATABASE IF EXISTS pixdb");
+		statments.executeUpdate("CREATE DATABASE pixdb");
+		statments.executeUpdate("USE pixdb");
+		statments.executeUpdate(
+				"CREATE TABLE users (userid INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, phonenumber VARCHAR(30) NOT NULL UNIQUE, deviceId VARCHAR(30) NOT NULL UNIQUE,reg_date TIMESTAMP,status TINYINT(1),amountsmssend INT(5))");
+		statments.executeUpdate(
+				"CREATE TABLE picturesInfo (pictureid INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, geodata Varchar(30), upvotes int(6), downvotes int(6))");
+		statments.executeUpdate("CREATE TABLE picturesData (pictureid INT(6) PRIMARY KEY, data LONGTEXT NOT NULL)");
+	}
+}

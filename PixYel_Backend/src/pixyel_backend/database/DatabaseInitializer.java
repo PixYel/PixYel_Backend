0b8025027957371b28f8f87099657d962b4ba529
Da_Groove <Da_Groove@172.31.3.78>
2016-10-20 14:11:50 +0200

    Comments-table created, userid renamed -> id, layout changes

--- PixYel_Backend/src/pixyel_backend/database/DatabaseInitializer.java
+++ PixYel_Backend/src/pixyel_backend/database/DatabaseInitializer.java
@@ -13,31 +13,41 @@ public class DatabaseInitializer {
         statements.executeUpdate("USE pixdb");
 
         statements.executeUpdate("CREATE TABLE users ("
-                + "userid INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, "
-                + "phonenumber INT(30) NOT NULL UNIQUE, "
-                + "deviceId VARCHAR(80) NOT NULL UNIQUE,"
-                + "reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP, "
-                + "publickey TEXT, "
-                + "status TINYINT(1) DEFAULT '0',"
-                + "amountsmssend INT(5) DEFAULT '0')"
+                + "id               INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, "
+                + "phonenumber      INT(30) NOT NULL UNIQUE, "
+                + "deviceId         VARCHAR(80) NOT NULL UNIQUE,"
+                + "reg_date         TIMESTAMP DEFAULT CURRENT_TIMESTAMP, "
+                + "publickey        TEXT, "
+                + "status           TINYINT(1) DEFAULT '0',"
+                + "amountsmssend    INT(5) DEFAULT '0')"
         );
 
         statements.executeUpdate("CREATE TABLE picturesInfo ("
-                + "pictureid INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, "
-                + "x_Coordinate DOUBLE, "
-                + "y_coordinate DOUBLE, "
-                + "upload_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP, "
-                + "upvotes INT(6), "
-                + "downvotes INT(6), "
-                + "flags INT(6), "
-                + "userid INT(6), "
-                + "commentId MEDIUMTEXT)"
+                + "pictureid        INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, "
+                + "x_Coordinate     DOUBLE NOT NULL, "
+                + "y_coordinate     DOUBLE NOT NULL, "
+                + "upload_date      TIMESTAMP DEFAULT CURRENT_TIMESTAMP, "
+                + "upvotes          INT(6) DEFAULT '0', "
+                + "downvotes        INT(6) DEFAULT '0', "
+                + "flags            INT(6) DEFAULT '0', "
+                + "userid           INT(6) NOT NULL, "
+                + "commentId        MEDIUMTEXT)"
         );
 
         statements.executeUpdate("CREATE TABLE picturesData ("
-                + "pictureid INT(6) PRIMARY KEY, "
-                + "data LONGTEXT NOT NULL)"
+                + "pictureid        INT(6) PRIMARY KEY, "
+                + "data             LONGTEXT NOT NULL)"
         );
+        
+        statements.executeUpdate("CREATE TABLE comments("
+                + "commentid        INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, "
+                + "pictureid        INT(6) NOT NULL, "
+                + "userid           INT(6) NOT NULL, "
+                + "comment          VARCHAR NOT NULL, "
+                + "comment_date     TIMESTAMP DEFAULT CURRENT_TIMESTAMP, "
+                + "flags            INT(6) DEFAULT '0')"
+        );
+        
         statements.close();
         conn.close();
     }

5e67e8b3e02d09e83db17dcad75275222807fd1e
Yannjoel <yannj96@googlemail.com>
2016-10-25 17:11:38 +0200

    New Exceptions

---
+++ PixYel_Backend/src/pixyel_backend/database/Exceptions/UserCreationException.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pixyel_backend.database.Exceptions;
+
+/**
+ *
+ * @author Yannick
+ */
+public class UserCreationException extends Exception{
+    
+}

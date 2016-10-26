9a814b16fd0fc23221b69a2d24fc448d303a7b75
Sharknoon <frank.josua@gmail.com>
2016-10-26 12:14:43 +0200

    new log for better logging (nss ;) )

--- PixYel_Backend/src/pixyel_backend/Log.java
+++ PixYel_Backend/src/pixyel_backend/Log.java
@@ -5,6 +5,10 @@
  */
 package pixyel_backend;
 
+import java.io.PrintStream;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -14,17 +18,56 @@ import java.util.logging.Logger;
  */
 public class Log {
 
-    private final static Logger LOG = Logger.getLogger(Logger.class.getName());
+    static int maxLengthOfClassName = 0;
 
-    public static void logInfo(String logMessage) {
-        LOG.log(Level.INFO, logMessage);
-    }
-    
-    public static void logError(String logMessage){
-        LOG.log(Level.SEVERE,logMessage);
+    public static void logInfo(String logMessage, Class<?> clasS) {
+        String toPrint = getClassNameWithDate(clasS);
+        toPrint += "INFO:    " + logMessage;
+        System.out.println(toPrint);
     }
 
-    public static void logWarning(String logMessage) {
-        LOG.log(Level.WARNING, logMessage);
+    public static void logError(String logMessage, Class<?> clasS) {
+        String toPrint = getClassNameWithDate(clasS);
+        toPrint += "ERROR:   " + logMessage;
+        System.err.println(toPrint);
+    }
+
+    public static void logWarning(String logMessage, Class<?> clasS) {
+        String toPrint = getClassNameWithDate(clasS);
+        toPrint += "WARNING: " + logMessage;
+        System.out.println(toPrint);
+    }
+
+    /**
+     *
+     * @param clasS
+     * @param color 0 = black, 1 = red
+     */
+    private static String getClassNameWithDate(Class<?> clasS) {
+        String result = "";
+        int tempLength;
+        if ((tempLength = clasS.getTypeName().length()) > maxLengthOfClassName) {
+            maxLengthOfClassName = tempLength;
+        }
+        if (maxLengthOfClassName % 2 == 0) {
+            for (int i = 0; i < (maxLengthOfClassName - tempLength) / 2; i++) {
+                result += (" ");
+            }
+            result += ("[" + clasS.getTypeName() + "]");
+            for (int i = 0; i < (maxLengthOfClassName - tempLength) / 2; i++) {
+                result += (" ");
+            }
+        } else {
+            for (int i = 0; i < ((maxLengthOfClassName - tempLength) / 2) - 1; i++) {
+                result += (" ");
+            }
+            result += ("[" + clasS.getTypeName() + "]");
+            for (int i = 0; i < (maxLengthOfClassName - tempLength) / 2; i++) {
+                result += (" ");
+            }
+        }
+        DateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy HH:mm:ss");
+        result += " [" + dateFormat.format(new Date()) + "] ";
+        return result;
     }
 }

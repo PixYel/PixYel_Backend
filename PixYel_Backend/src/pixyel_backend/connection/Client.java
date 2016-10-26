8729b90ded86bd6a83829ffb3d9f16b6af30f9b7
Administrator <Administrator@WIN-0OU4BUCE509>
2016-10-26 14:51:07 +0200

    HUGE comment mountains

--- PixYel_Backend/src/pixyel_backend/connection/Client.java
+++ PixYel_Backend/src/pixyel_backend/connection/Client.java
@@ -16,8 +16,6 @@ import java.util.logging.Logger;
 import pixyel_backend.Log;
 import pixyel_backend.connection.compression.Compression;
 import pixyel_backend.connection.encryption.Encryption;
-import pixyel_backend.database.exceptions.UserCreationException;
-import pixyel_backend.database.exceptions.UserNotFoundException;
 import pixyel_backend.database.objects.User;
 import pixyel_backend.xml.XML;
 
@@ -27,9 +25,9 @@ import pixyel_backend.xml.XML;
  */
 public class Client implements Runnable {
 
-    Socket socket;
+    private final Socket socket;
     private static final String SERVERPRIVATEKEY = "MIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQCYbw5+EmuQ33+tcuHtq7JQ9yrtTGZgyh1izAKFjyEgyBk7ib5zE5ainE6m1QBh52MDhkAoan8/WDqfphpe+09CohuLsbmgJPi9uHTNgWAjfA0ImUq1RfWBvq4JBbes+KcA1w+2VmgjJ6ACyf85bQUxm1zXHw83QfUxBOVwJy1d0l2i26WN5iuI7WWRF9vcayi0shiNyIyogwoZBFDZGGPzVufE/0XJt025UiOVLpF/H1/NpnYEoh2bs7v8POf1NxJkQ5l05JLsIcwwqvXg7bqD5tHYeIZbHwBs4fbM/cMylH39jwZSuM7pA+oD4aisgIZhbEiWtybiJjHzvjQWwNpBAgMBAAECggEAa9DRPVamAlgypGnHZyW0ABRyplkNaJRMaJ7HgdQUqG0fe78Xl6lZODa6YsHxfU39+HYyVuPMkO9Et7rymA/Epeqm9Q2Fr8G2teoLo3dImpCZX2WdM84Bsf0+d1815QASj0ON93fyPDtAr/hTrzhvHgE3j2iGiJz8YemGpubu7ZZPVy89hwNhFcLZxEj8ZTI07nuy+uSiOaJQhrIl8jqH/Y099ex838zP4Bkqo321q5Rb0eYKB+cAB2oU6GQ411mARzkOwB6TI+PHB4l6MPryR24fO0JokiRafs178K3sRIEG2ZwC6VS/5cYDzoksEC/ztmmVV8UBmZXgTJqlQ9OjYQKBgQDTKXQw4pg7V9dV1ku+VrMc09EkI76SX+gZ8+qu4ogM4BFx7ifuwORRAv8pvvKXLN1N0o1br4JgEHnspAZdBOi/GqSnk93Mzdyu4eVeNJsWCW9uQG+x3JutsI4uTipGL3Ujf3RNVVBLz8Mn09iWKBq9x+k3DO6NS7ZsYIK7ueDyBQKBgQC4zTPeoGicyAiONGRwatS94gB7O0OB1OmR0ogjgHxKnZYji1fZZB3MtvspIThFbFM899+iRNwCm9DBSswzmAHCrUEdMVSapuLIcDnFFBD4bsGIi1WQSR0/sq9KXeDb7y2mXDOBdQKT+/YBYCIFSc0uMYuTKPJETXZbAZIqn1dQDQKBgFDmDNG9vjzeBm2oUBR3+t8Md4+08gn6HF31CPA/cYgdQBG6ACqEU/SFhWRk92PbmF18URPTRcuwBEYZAScZ/mFYv8RD7jHMnMOTX+Cbnt7udnZy0Vf/ANWmUpKC98cz31jeN5x5Fu40hrv3eX7tlnEm6b5hgfM4Eoeq4esx53BRAoGAfowWbucNe/8GzMpX1Rty3yx6A+kLS0bPQwoWK6l6c2YCQAjVeYozVdIfn4SeEfwh6+gZffcFacvlYdekidgXrIYTxrfXJagOOA+Rn8Ej9dtyL9yqFQncO5hSrOwoZLMeYYNVydqkj03EoiCAa3qkRHOtDnLFo7wfxiYHP6Spl6kCgYAyE7LN0YZFqHqsvYhLcJVfQtW2kEJp/1HnXIwMHLqDJVeQf3+JPGXZ9vAaonz1bKL9LNdkPXGjVYwxJ99HEdahwgD9TGs57MjfliF4tG9h38KCpSiGD1PXRq3/GJt2XsG5xK5gglBOauAVWRDZAZNqDCNiTIQtw3eXatyZPklrSw==";
-    User userdata;
+    protected User userdata;
 
     public Client(Socket socket) {
         this.socket = socket;
@@ -72,7 +70,11 @@ public class Client implements Runnable {
         }
     }
 
-    public void onStringReceived(String receivedString) {
+    /**
+     * This method is going to be called as soon as a string was send to this client
+     * @param receivedString The received encrypted and compressed string
+     */
+    private void onStringReceived(String receivedString) {
         if (receivedString != null) {
             //------------TEMP-START-------------
             if (receivedString.equals("echo")) {
@@ -91,42 +93,44 @@ public class Client implements Runnable {
                 String decrypted = Encryption.decrypt(receivedString, SERVERPRIVATEKEY);
                 String decompressed = Compression.decompress(decrypted);
                 XML xml = XML.openXML(decompressed);
-                if (!xml.getName().equals("login")) {
-                    Command.onCommandReceived(this, userdata, xml);
-                } else {
-                    login(xml);
-                }
+                Command.onCommandReceived(this, xml);
             } catch (XML.XMLException ex) {
                 Log.logError("Client has send an invalid XML: " + ex, this);
             }
         } else {
-            Log.logError("String ist NULL", this);
+            Log.logError("String is NULL", this);
         }
     }
 
-    public void login(XML xml) {
-        try {
-            userdata = User.getUser(xml.getFirstChild("storeid").getContent());
-        } catch (UserNotFoundException | UserCreationException ex) {
-            try {
-                userdata = User.addNewUser(xml.getFirstChild("storeid").getContent());
-            } catch (UserCreationException ex1) {
-                Log.logError("Could not create User: " + ex1, this);
-            }
-        }
-        userdata.setPublicKey(xml.getFirstChild("publickey").getContent());
-    }
-
+    /**
+     * Call this method to disconnect the client from this server
+     * @param expected Is the disconnect expected?
+     */
     public void disconnect(boolean expected) {
-        Connection.removeFromClientList(this);
+        userdata.closeDbConnection();
+        Connection.disconnect(socket.hashCode());
         try {
             socket.close();
         } catch (Exception e) {
-            Log.logError("Could not stop ", this);
+            Log.logError("Could not close the clients socket", this);
         }
-        Log.logInfo("Client stopped", this);
     }
 
+    /**
+     * Returns the name (Store ID) of this client or the hash code of the socket, if its not yet logged in
+     * @return The name (Store ID) of this client or the hash code of the socket, if its not yet logged in
+     */
+    public String getName() {
+        if (userdata != null) {
+            return userdata.getStoreID();
+        } else {
+            return String.valueOf(socket.hashCode());
+        }
+    }
+
+    /**
+     * The Class, which looks for Strings from the client
+     */
     public class ClientInputListener implements Runnable {
 
         @Override

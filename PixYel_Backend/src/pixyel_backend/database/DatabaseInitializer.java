dc17755bee31c84c0d3e05fd62ed226307585562
Yannjoel <yannj96@googlemail.com>
2016-10-27 15:22:32 +0200

    Bugfix

--- PixYel_Backend/src/pixyel_backend/database/DatabaseInitializer.java
+++ PixYel_Backend/src/pixyel_backend/database/DatabaseInitializer.java
@@ -40,7 +40,7 @@ public class DatabaseInitializer {
                 + "downvotes        INT(6) DEFAULT '0', "
                 + "flags            TINYTEXT, "
                 + "userid           INT(6) NOT NULL, "
-                + "commentId        MEDIUMTEXT)"
+                + "commentIds       TINYTEXT)"
         );
 
         statements.executeUpdate("CREATE TABLE picturesData ("
@@ -54,7 +54,7 @@ public class DatabaseInitializer {
                 + "userid           INT(6) NOT NULL, "
                 + "comment          VARCHAR(1200) NOT NULL, "
                 + "comment_date     TIMESTAMP DEFAULT CURRENT_TIMESTAMP, "
-                + "flags            TINYTEXT "
+                + "flags            TINYTEXT"
         );
         
         statements.close();

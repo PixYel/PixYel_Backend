6abd5e274d09c2244c0d1002f3a6d9152d81e3c2
Da_Groove <Da_Groove@172.31.3.78>
2016-10-27 14:31:49 +0200

    Added more functionality (constructor) to the Comment object

--- PixYel_Backend/src/pixyel_backend/database/objects/Comment.java
+++ PixYel_Backend/src/pixyel_backend/database/objects/Comment.java
@@ -5,27 +5,58 @@
  */
 package pixyel_backend.database.objects;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Arrays;
 import java.util.Date;
+import java.util.List;
+import pixyel_backend.Log;
+import pixyel_backend.database.DbConnection;
+import pixyel_backend.database.exceptions.CommentCreationException;
+import pixyel_backend.database.exceptions.CommentNotFoundException;
+import pixyel_backend.database.exceptions.DbConnectionException;
 
 /**
  *
  * @author Groove
  */
 public class Comment {
+
     private final int commentId;
     private int pictureId;
     private int userId;
     private String comment;
     private final Date commentDate;
     private int flags;
-    
-    public Comment(int commentId, int pictureId, int userId, String comment, Date commentDate, int flags){
-        this.commentId = commentId;
-        this.pictureId = pictureId;
-        this.userId = userId;
-        this.comment = comment;
-        this.commentDate = commentDate;
-        this.flags = flags;
+    private List<String> flaggedBy;
+    private DbConnection con;
+
+    public Comment(int commentId) throws CommentCreationException {
+
+        try {
+            this.con = new DbConnection();
+            PreparedStatement statement = con.getPreparedStatement("SELECT * FROM comments WHERE commentid LIKE ?");
+            statement.setInt(1, commentId);
+            ResultSet result = statement.executeQuery();
+            if (result == null || !result.isBeforeFirst()) {
+                throw new CommentNotFoundException();
+            }
+            result.next();
+            this.commentId = commentId;
+            this.pictureId = result.getInt("pictureid");
+            this.userId = result.getInt("userid");
+            this.comment = result.getString("comment");
+            this.commentDate = result.getDate("comment_date");
+            String flaggedByAsString = result.getString("flags");
+            this.flaggedBy = Arrays.asList(flaggedByAsString);
+            this.flags = this.flaggedBy.size();
+
+            
+        } catch (SQLException | DbConnectionException ex) {
+            Log.logError("Could not read Commentinformation from database - rootcause: " + ex.getMessage(), this);
+            throw new CommentCreationException();
+        }
     }
 
     /**
@@ -90,8 +121,29 @@ public class Comment {
     public int getFlags() {
         return flags;
     }
-    
-    public void addFlag(){
+
+    public void addFlag() {
         this.flags++;
+        updateCommentValue("flags", this.flags);
+    }
+
+    public boolean isFlaggedBy(int userid) {
+        return (this.flaggedBy.contains(String.valueOf(userid)));
+    }
+
+    /**
+     *
+     * @param column
+     * @param toValue
+     */
+    private void updateCommentValue(String column, int toValue) {
+        try {
+            PreparedStatement sta = con.getPreparedStatement("UPDATE users SET " + column + " = ? WHERE id LIKE " + this.commentId);
+            sta.setInt(1, toValue);
+            sta.execute();
+            sta.close();
+        } catch (SQLException ex) {
+            Log.logError("couldnt update user value \"" + column + "\" - rootcause:" + ex.getMessage(), this);
+        }
     }
 }

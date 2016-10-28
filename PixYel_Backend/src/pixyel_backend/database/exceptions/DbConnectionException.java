51b1da689125741e1e05abc89e4304064104b69c
Yannjoel <yannj96@googlemail.com>
2016-10-28 13:52:59 +0200

    Added new Methods

--- PixYel_Backend/src/pixyel_backend/database/exceptions/DbConnectionException.java
+++ PixYel_Backend/src/pixyel_backend/database/exceptions/DbConnectionException.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package pixyel_backend.database.exceptions;
+package pixyel_backend.database.Exceptions;
 
 /**
  *

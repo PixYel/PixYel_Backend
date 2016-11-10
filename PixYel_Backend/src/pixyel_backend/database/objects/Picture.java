487898690b878a1aa30464c59f87caff7d6819f0
Yannjoel <yannj96@googlemail.com>
2016-11-10 17:14:49 +0100

    Implementation of the basic functions

--- PixYel_Backend/src/pixyel_backend/database/objects/Picture.java
+++ PixYel_Backend/src/pixyel_backend/database/objects/Picture.java
@@ -5,6 +5,7 @@
  */
 package pixyel_backend.database.objects;
 
+import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -12,38 +13,43 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
-import pixyel_backend.database.DbConnection;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import pixyel_backend.Log;
+import pixyel_backend.database.MysqlConnector;
+import pixyel_backend.database.SqlUtils;
+import pixyel_backend.database.exceptions.PictureLoadException;
+import pixyel_backend.database.exceptions.PictureUploadExcpetion;
 
 /**
- *
  * @author Da_Groove
  */
 public class Picture {
 
-    private int id;
-    private String data;
-    private double longitude;
-    private double latitude;
-    private Date timestamp;
-    private int upvotes;
-    private int downvotes;
-    private int flags;
-    private List<String> flaggedBy;
-    private int userId;
-    private String commentIds;
+    private final int id;
+    private final String data;
+    private final double longitude;
+    private final double latitude;
+    private final Date timestamp;
+    private final int upvotes;
+    private final int downvotes;
+    private final int flags;
+    private final List<String> flaggedBy;
+    private final int userId;
+    private int rating;
 
-    public Picture(int pictureId, DbConnection con) throws SQLException {
+    public Picture(int pictureId) throws PictureLoadException{
         this.id = pictureId;
-
+        try{
         //get Data
-        PreparedStatement sta = con.getPreparedStatement("SELECT data FROM picturesData WHERE pictureid LIKE ?");
+        PreparedStatement sta = MysqlConnector.getConnection().prepareStatement("SELECT data FROM picturesData WHERE pictureid LIKE ?");
         sta.setInt(1, this.id);
         ResultSet result = sta.executeQuery();
         result.next();
         this.data = result.getString("data");
 
         //get Info
-        sta = con.getPreparedStatement("SELECT * FROM picturesInfo WHERE pictureid LIKE ?");
+        sta = MysqlConnector.getConnection().prepareStatement("SELECT * FROM picturesInfo WHERE pictureid LIKE ?");
         sta.setInt(1, this.id);
         result = sta.executeQuery();
         result.next();
@@ -63,11 +69,137 @@ public class Picture {
             this.flaggedBy = new ArrayList();
             this.flags = 0;
         }
-        
+
         //get commentIds
-        if(result.getString("commentids") != null) {
+        if (result.getString("commentids") != null) {
             String commentedByAsString = result.getString("commentids");
             //this.commentIds = Arrays.asList(commentedByAsString);
         }
+        }catch(Exception ex){
+            throw new PictureLoadException();
+        }
+    }
+
+    public static synchronized Picture addNewPicture(int userId, String pictureData, double longitude, double latitude) throws PictureUploadExcpetion, PictureLoadException {
+        int pictureId;
+        try {
+            Connection con = MysqlConnector.getConnection();
+            PreparedStatement statement;
+            if (pictureData != null && pictureData.length() >= 1) {
+                statement = con.prepareStatement("INSERT INTO picturesInfo (longditude, latitude, userid) VALUES(?,?,?)");
+                statement.setDouble(1, longitude);
+                statement.setDouble(2, latitude);
+                statement.setInt(3, userId);
+                statement.executeUpdate();
+                ResultSet rs = statement.executeQuery("SELECT MAX(id) as maxid FROM pictureInfo");
+                //get Id as reference for the picturedata
+                rs.next();
+                pictureId = rs.getInt("maxid");
+                pictureData = SqlUtils.escapeString(pictureData);
+                statement = con.prepareStatement("INSERT INTO picturesData (picutreid, data) VALUES(?,?)");
+                statement.setInt(1, pictureId);
+                statement.setString(2, pictureData);
+                statement.close();
+            } else {
+                Log.logInfo("Failed to add picture for user \"" + userId + "\" - rootcause: picturedata is NULL or to empty string", Picture.class);
+                throw new PictureUploadExcpetion();
+            }
+        } catch (SQLException ex) {
+            Log.logError("Could add new picture to database - rootcause: " + ex.getMessage(), Picture.class);
+            throw new PictureUploadExcpetion();
+        }
+        return new Picture(pictureId);
+    }
+    
+    public static Picture getPictureById(int id) throws PictureLoadException{
+        return new Picture(id);
+    }
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @return the data
+     */
+    public String getData() {
+        return data;
+    }
+
+    /**
+     * @return the longitude
+     */
+    public double getLongitude() {
+        return longitude;
+    }
+
+    /**
+     * @return the latitude
+     */
+    public double getLatitude() {
+        return latitude;
+    }
+
+    /**
+     * @return the timestamp
+     */
+    public Date getTimestamp() {
+        return timestamp;
+    }
+
+    /**
+     * @return the upvotes
+     */
+    public int getUpvotes() {
+        return upvotes;
+    }
+
+    /**
+     * @return the downvotes
+     */
+    public int getDownvotes() {
+        return downvotes;
+    }
+
+    /**
+     * @return the flags
+     */
+    public int getFlags() {
+        return flags;
+    }
+
+    /**
+     * @return the flaggedBy
+     */
+    public List<String> getFlaggedBy() {
+        return flaggedBy;
+    }
+
+    /**
+     * @return the userId
+     */
+    public int getUserId() {
+        return userId;
+    }
+
+    /**
+     * @return the rating
+     */
+    public int getRating() {
+        return rating;
+    }
+
+    /**
+     * @param rating the rating to set
+     */
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    int getID() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 }

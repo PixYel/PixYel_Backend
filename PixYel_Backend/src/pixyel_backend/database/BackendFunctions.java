2a1a6c7d58940cc8421cd975f25645aec1e50cb3
Yannjoel <yannj96@googlemail.com>
2016-11-17 14:50:21 +0100

    Implmentation of methode getPictureList

--- PixYel_Backend/src/pixyel_backend/database/BackendFunctions.java
+++ PixYel_Backend/src/pixyel_backend/database/BackendFunctions.java
@@ -5,45 +5,99 @@
  */
 package pixyel_backend.database;
 
+import pixyel_backend.database.exceptions.NoPicturesFoundExcpetion;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Arrays;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import pixyel_backend.database.exceptions.CommentCreationException;
-import pixyel_backend.database.exceptions.PictureUploadExcpetion;
+import pixyel_backend.Log;
+import pixyel_backend.database.dataProcessing.RankingCalculation;
+import pixyel_backend.database.exceptions.CommentNotFoundException;
+import pixyel_backend.database.exceptions.PictureLoadException;
 import pixyel_backend.database.objects.Comment;
+import pixyel_backend.database.objects.Coordinate;
 import pixyel_backend.database.objects.Picture;
-import pixyel_backend.xml.XML;
 
 /**
  *
  * @author Yannick
  */
 public class BackendFunctions {
-    public static List<Comment> getCommentsForPicutre(int pictureId) throws SQLException, CommentCreationException {
+
+    public static List<Comment> getCommentsForPicutre(int pictureId) {
         return Comment.getCommentsForPicutre(pictureId);
     }
 
-    public static List<Picture> getPicturesList(int xCordinate, int yCordinate) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    /**
+     * @param cord
+     * @return
+     * @throws pixyel_backend.database.exceptions.NoPicturesFoundExcpetion
+     */
+    public static List<Picture> getPicturesList(Coordinate cord) throws NoPicturesFoundExcpetion {
+        return getPicturesList(cord, 20);
     }
 
     /**
-     * 
-     * @param listAsString , separated list of all id example: 1,2,4,6
-     * @return 
+     * @param cord
+     * @param searchDistance in km
+     * @return
+     * @throws pixyel_backend.database.exceptions.NoPicturesFoundExcpetion
      */
-    public static Picture getPicturesData(String listAsString) {
+    public static List<Picture> getPicturesList(Coordinate cord, int searchDistance) throws NoPicturesFoundExcpetion {
+        List<Picture> pictureList = new LinkedList();
+        List<Coordinate> searchArea = cord.getSearchArea(searchDistance);
+        try (PreparedStatement sta = MysqlConnector.getConnection().prepareStatement("SELECT id FROM picturesInfo WHERE (longitude BETWEEN ? AND ?) AND (latitude BETWEEN ? AND ?)")) {
+            sta.setDouble(1, searchArea.get(0).getLongitude());
+            sta.setDouble(2, searchArea.get(1).getLongitude());
+            sta.setDouble(3, searchArea.get(0).getLatitude());
+            sta.setDouble(4, searchArea.get(1).getLatitude());
+            ResultSet result = sta.executeQuery();
+
+            if (result == null || !result.isBeforeFirst()) {
+                throw new NoPicturesFoundExcpetion();
+            } else {
+                while (result.next()) {
+                    Picture pic = Picture.getPictureById(result.getInt("id"));
+                    pic.setRanking(RankingCalculation.calculateRanking(pic, cord));
+                    pictureList.add(pic);
+                }
+            }
+        } catch (SQLException ex) {
+            Log.logWarning(ex.toString(), BackendFunctions.class);
+            throw new NoPicturesFoundExcpetion();
+        } catch (PictureLoadException ex) {
+            Log.logWarning(ex.getMessage(), BackendFunctions.class);
+        }
+        pictureList.sort((Picture pic1, Picture pic2) -> Integer.compare(pic1.getRanking(), pic2.getRanking()));
+        return pictureList;
+    }
+
+    /**
+     *
+     * @param listAsString , separated list of all id example: 1,2,4,6
+     * @return A map which contains all requested pictureData with their Id as
+     * keys
+     */
+    public static Map<Integer, String> getPicturesData(String listAsString) {
+        HashMap<Integer, String> pictureList = new HashMap<>();
         List<String> allRequestedPictures = Arrays.asList(listAsString);
         for (String currentPictureIdAsString : allRequestedPictures) {
-            ////Picutres holen zu XML hinzufügen 
+            Integer currentPictureId = Integer.valueOf(currentPictureIdAsString);
+            String currentPictureData;
+            try {
+                currentPictureData = Picture.getDataForId(currentPictureId);
+            } catch (PictureLoadException ex) {
+                currentPictureData = null;
+            }
+            pictureList.put(currentPictureId, currentPictureData);
         }
-        return null;
+        return pictureList;
     }
-
-    public static void flagPicture(int pictureId) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
 }

0a55e268ec35ec1d6374e5352740bfb2d4a745ee
Da_Groove <Da_Groove@172.31.3.78>
2016-11-17 15:24:31 +0100

    Removed unnecessary stuff

--- PixYel_Backend/src/pixyel_backend/database/Test.java
+++ PixYel_Backend/src/pixyel_backend/database/Test.java
@@ -3,14 +3,7 @@ package pixyel_backend.database;
 public class Test {
 
 	public static void main(String[] args) {
-		try {
-			DatabaseInitializer.init();
-		} catch (Exception e) {
-			System.out.println(e);
-		}
-		String testnum = "+015783978333";
-		
-		System.out.println();
+
 	}
 
 }

6e37993c275a87833c84eea0269da5bc685279ae
Josua Frank <Josua Frank@192.168.178.128>
2016-11-30 22:57:34 +0100

    first implementations on the desktop

--- PixYel_Backend/src/pixyel_backend/userinterface/Translations.java
+++ PixYel_Backend/src/pixyel_backend/userinterface/Translations.java
@@ -30,6 +30,11 @@ public class Translations {
     public static final int LOGIN_WRONGLOGIN = 3;
 
     //Desktop
+    public static final int DESKTOP_CONSOLE = 100;
+    public static final int DESKTOP_ONLINE_MONITOR = 101;
+    public static final int DESKTOP_USER_MANAGMENT = 102;
+    public static final int DESKTOP_LOGOUT = 103;
+
     public static void init() {
         //**********************************//
         //           TRANSLATIONS           //
@@ -39,12 +44,20 @@ public class Translations {
         english.put(LOGIN_USERNAME, "Username");
         english.put(LOGIN_PASSWORD, "Password");
         english.put(LOGIN_WRONGLOGIN, "Wrong Username or password");
+        english.put(DESKTOP_CONSOLE, "Console");
+        english.put(DESKTOP_ONLINE_MONITOR, "Online Monitor");
+        english.put(DESKTOP_USER_MANAGMENT, "User Management");
+        english.put(DESKTOP_LOGOUT, "Log out");
 
         //German
         german.put(LOGIN_LOGINBUTTON, "Anmelden");
         german.put(LOGIN_USERNAME, "Benutzername");
         german.put(LOGIN_PASSWORD, "Passwort");
         german.put(LOGIN_WRONGLOGIN, "Falscher Benutzername oder Passwort");
+        german.put(DESKTOP_CONSOLE, "Konsole");
+        german.put(DESKTOP_ONLINE_MONITOR, "Serverüberwachung");
+        german.put(DESKTOP_USER_MANAGMENT, "Benutzerverwaltung");
+        german.put(DESKTOP_LOGOUT, "Abmelden");
     }
 
     public static String get(int KEYWORD) {
--- PixYel_Backend/src/pixyel_backend/userinterface/UIs/DesktopUI/Desktop.java
+++ PixYel_Backend/src/pixyel_backend/userinterface/UIs/DesktopUI/Desktop.java
@@ -5,13 +5,24 @@
  */
 package pixyel_backend.userinterface.UIs.DesktopUI;
 
+import com.vaadin.server.ExternalResource;
 import com.vaadin.server.FileResource;
+import com.vaadin.server.Resource;
 import com.vaadin.ui.AbsoluteLayout;
+import com.vaadin.ui.Alignment;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.Button.ClickListener;
+import com.vaadin.ui.Component;
+import com.vaadin.ui.GridLayout;
 import com.vaadin.ui.Image;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.Notification;
 import com.vaadin.ui.UI;
+import com.vaadin.ui.VerticalLayout;
 import com.vaadin.ui.Window;
 import java.util.Collection;
 import pixyel_backend.Log;
+import pixyel_backend.userinterface.Translations;
 import pixyel_backend.userinterface.ressources.Ressources;
 
 /**
@@ -26,6 +37,7 @@ public class Desktop {
         //Notification.show("Showing Desktop");
         closeAllWindows();
         layout.addComponent(getImage(), "top:0px; left: 0px;");
+        layout.addComponent(getTaskBar(), "top: 80%; left: 0%; right: 0%, bottom: 0%");
 
         layout.setSizeFull();
         UI.getCurrent().setContent(layout);
@@ -42,10 +54,50 @@ public class Desktop {
         try {
             image.setSource(new FileResource(Ressources.getRessource("desktop_background.jpg")));
         } catch (Ressources.RessourceNotFoundException ex) {
-            Log.logError("Could not set the background for the desktop: ", Desktop.class);
+            Log.logError("Could not set the background for the desktop: " + ex, Desktop.class);
         }
         image.setSizeFull();
         return image;
+    }
+
+    private static Component getTaskBar() {
+        GridLayout gridLayout = new GridLayout(4, 1);
+        gridLayout.setSizeFull();
+
+        VerticalLayout appConsole = getAppButtons(Translations.get(Translations.DESKTOP_CONSOLE), new ExternalResource("http://vaadin.com/image/user_male_portrait?img_id=44268&t=1251193981449"));
+        gridLayout.addComponent(appConsole, 0, 0);
+        gridLayout.setComponentAlignment(appConsole, Alignment.MIDDLE_CENTER);
+
+        VerticalLayout appOnlineMonitor = getAppButtons(Translations.get(Translations.DESKTOP_ONLINE_MONITOR), new ExternalResource("http://vaadin.com/image/user_male_portrait?img_id=44268&t=1251193981449"));
+        gridLayout.addComponent(appOnlineMonitor, 1, 0);
+        gridLayout.setComponentAlignment(appOnlineMonitor, Alignment.MIDDLE_CENTER);
+
+        VerticalLayout appUserManagement = getAppButtons(Translations.get(Translations.DESKTOP_USER_MANAGMENT), new ExternalResource("http://vaadin.com/image/user_male_portrait?img_id=44268&t=1251193981449"));
+        gridLayout.addComponent(appUserManagement, 2, 0);
+        gridLayout.setComponentAlignment(appUserManagement, Alignment.MIDDLE_CENTER);
+
+        VerticalLayout appLogout = getAppButtons(Translations.get(Translations.DESKTOP_LOGOUT), new ExternalResource("http://vaadin.com/image/user_male_portrait?img_id=44268&t=1251193981449"));
+        gridLayout.addComponent(appLogout, 3, 0);
+        gridLayout.setComponentAlignment(appLogout, Alignment.MIDDLE_CENTER);
+        return gridLayout;
+    }
+
+    private static VerticalLayout getAppButtons(String caption, Resource icon) {
+        Button button = new Button();
+        //button.setStyleName(ValoTheme.BUTTON_BORDERLESS_COLORED);
+        button.setIcon(new ExternalResource("http://vaadin.com/image/user_male_portrait?img_id=44268&t=1251193981449"));
+        button.addListener((ClickListener) (Button.ClickEvent event) -> {
+            Notification.show("CLICK!");
+        });
+        button.setWidth("140px");
+        button.setHeight("140px");
+
+        Label label = new Label(Translations.get(Translations.DESKTOP_ONLINE_MONITOR));
+        VerticalLayout verticalLayout = new VerticalLayout(button, label);
+        verticalLayout.setSizeFull();
+        verticalLayout.setComponentAlignment(button, Alignment.MIDDLE_CENTER);
+        verticalLayout.setComponentAlignment(label, Alignment.MIDDLE_CENTER);
+        return verticalLayout;
     }
 
 }

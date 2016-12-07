b05e8eb3ae1617ac74dd49a61a21206730e8cb56
Josua Frank <Josua Frank@192.168.178.128>
2016-12-07 08:05:28 +0100

    more work done on the desktop

---
+++ PixYel_Backend/src/pixyel_backend/userinterface/UIs/DesktopUI/ConsoleWindow.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pixyel_backend.userinterface.UIs.DesktopUI;
+
+import com.vaadin.ui.Window;
+
+/**
+ *
+ * @author Josua Frank
+ */
+public class ConsoleWindow extends Window {
+    
+    public ConsoleWindow() {
+        setContent(this);
+    }
+    
+}

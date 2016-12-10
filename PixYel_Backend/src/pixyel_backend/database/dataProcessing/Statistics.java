13f9e9dd04f112aaa6f8ac350e74f6df87512abd
Yannjoel <yannj96@googlemail.com>
2016-12-10 10:20:19 +0100

    Added new Statstics

--- PixYel_Backend/src/pixyel_backend/database/dataProcessing/Statistics.java
+++ PixYel_Backend/src/pixyel_backend/database/dataProcessing/Statistics.java
@@ -8,6 +8,9 @@ package pixyel_backend.database.dataProcessing;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.sql.Timestamp;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
 import pixyel_backend.Log;
 import pixyel_backend.database.Columns;
 import pixyel_backend.database.MysqlConnector;
@@ -19,10 +22,10 @@ import pixyel_backend.database.MysqlConnector;
 public class Statistics {
 
     /**
-     * 
+     *
      * @return the amount of registered user
      */
-    public static int countRegisteredUsers() {
+    public static int totallyRegisteredUsers() {
         try (Statement sta = MysqlConnector.getConnection().createStatement(); ResultSet result = sta.executeQuery("SELECT COUNT(*) AS NumberOfUser FROM users;")) {
             result.next();
             return result.getInt(1);
@@ -33,10 +36,10 @@ public class Statistics {
     }
 
     /**
-     * 
+     *
      * @return the amount of banned users
      */
-    public static int countBannedUsers() {
+    public static int totallyBannedUsers() {
         try (Statement sta = MysqlConnector.getConnection().createStatement(); ResultSet result = sta.executeQuery("SELECT COUNT(*) AS NumberOfUser FROM users WHERE " + Columns.STATUS + " < 0;")) {
             result.next();
             return result.getInt(1);
@@ -47,11 +50,101 @@ public class Statistics {
     }
 
     /**
-     * 
+     *
      * @return the amount of all pictures that were posted
      */
-    public static int totallyPostedPictures() {
+    public static int totallyUploadedPictures() {
         try (Statement sta = MysqlConnector.getConnection().createStatement(); ResultSet result = sta.executeQuery("SELECT COUNT(*) AS NumberOfPictures FROM picturesInfo;")) {
+            result.next();
+            return result.getInt(1);
+        } catch (SQLException ex) {
+            Log.logError(ex.getMessage(), Statistics.class);
+            return -1;
+        }
+    }
+
+    /**
+     * Counts how many new Users were registrated within the last 24 hours
+     *
+     * @return
+     */
+    public int UserRegistrationInTheLastDay() {
+        return UserRegistrationInTheLastTime(1);
+    }
+
+    /**
+     * Counts how many new Users were registrated within the last week
+     *
+     * @return
+     */
+    public int UserRegistrationInTheLast7Days() {
+        return UserRegistrationInTheLastTime(7);
+    }
+
+    /**
+     * Counts how many new Users were registrated within the last week
+     *
+     * @return
+     */
+    public int UserRegistrationInTheLast30Days() {
+        return UserRegistrationInTheLastTime(30);
+    }
+
+    /**
+     * Counts how many new Users were registrated within a given time
+     *
+     * @param days
+     * @return
+     */
+    private int UserRegistrationInTheLastTime(int days) {
+        Instant instant = Instant.now().minus(days, ChronoUnit.DAYS);
+        Timestamp currentTimestamp = Timestamp.from(instant);
+        try (Statement sta = MysqlConnector.getConnection().createStatement(); ResultSet result = sta.executeQuery("SELECT COUNT(*) AS NumberOfPictures FROM users WHERE " + Columns.REGISTRATION_DATE + " > " + currentTimestamp)) {
+            result.next();
+            return result.getInt(1);
+        } catch (SQLException ex) {
+            Log.logError(ex.getMessage(), Statistics.class);
+            return -1;
+        }
+    }
+
+    /**
+     * Counts how many new Pictures were uploaded within the last week
+     *
+     * @return
+     */
+    public int PictureUploadsInTheLastDay() {
+        return PictureUploadsInTheLastTime(1);
+    }
+
+    /**
+     * Counts how many new Pictures were uploaded within the last week
+     *
+     * @return
+     */
+    public int PictureUploadsInTheLast7Days() {
+        return PictureUploadsInTheLastTime(7);
+    }
+
+    /**
+     * * Counts how many new Pictures were uploaded within the last week
+     *
+     * @return
+     */
+    public int PictureUploadsInTheLast30Days() {
+        return PictureUploadsInTheLastTime(30);
+    }
+
+    /**
+     * Counts how many new Pictures were uploaded within a given time
+     *
+     * @param days
+     * @return
+     */
+    private int PictureUploadsInTheLastTime(int days) {
+        Instant instant = Instant.now().minus(days, ChronoUnit.DAYS);
+        Timestamp currentTimestamp = Timestamp.from(instant);
+        try (Statement sta = MysqlConnector.getConnection().createStatement(); ResultSet result = sta.executeQuery("SELECT COUNT(*) AS NumberOfPictures FROM picturesInfo WHERE " + Columns.UPLOAD_DATE + " > " + currentTimestamp)) {
             result.next();
             return result.getInt(1);
         } catch (SQLException ex) {

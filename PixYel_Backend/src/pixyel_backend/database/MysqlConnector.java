a08c64fa5840eb1840d46f1645bf41ef6b15d610
Yannjoel <yannj96@googlemail.com>
2016-12-13 10:42:11 +0100

    Bugfixes

--- PixYel_Backend/src/pixyel_backend/database/MysqlConnector.java
+++ PixYel_Backend/src/pixyel_backend/database/MysqlConnector.java
@@ -16,11 +16,6 @@ public class MysqlConnector {
      * which was set up by using the properties file
      */
     private static Connection CONNECTION = MysqlConnector.connectToProductivDatabaseUsingPropertiesFile();
-
-    public static void onstartup() throws SQLException{
-        CONNECTION.getNetworkTimeout();
-    }
-    
     
     /**
      * Returns the Connection pixyel_backend.database.MysqlConnector.CONNECTION
